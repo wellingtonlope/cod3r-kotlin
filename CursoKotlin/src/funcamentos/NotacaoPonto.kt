b4cb50b1393c3583c43fc918428d8be5f4e086fe
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-02-28 09:19:16 -0400

    dot notation

---
+++ CursoKotlin/src/funcamentos/NotacaoPonto.kt
@@ -0,0 +1,9 @@
+package funcamentos
+
+fun main() {
+    val a: Int = 33.dec()
+    var b: String = a.toString()
+
+    println("Int: $a")
+    println("Primeiro char da string b é: ${b.first()}")
+}
