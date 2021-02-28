eb106680c2107da81840eb272572690ea4eb95f0
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-02-28 08:29:56 -0400

    careful with type inference 2

--- CursoKotlin/src/funcamentos/CuidadoInferencia1.kt
+++ CursoKotlin/src/funcamentos/CuidadoInferencia1.kt
@@ -4,6 +4,7 @@ fun main() {
     /*
      * without type, this code don't work,
      * because kotlin is strongly typed :)
+     * example: var a;
      */
     var a: Int
     var b = 2
---
+++ CursoKotlin/src/funcamentos/CuidadoInferencia2.kt
@@ -0,0 +1,14 @@
+package funcamentos
+
+fun main() {
+    /*
+     * you can't change a variable type
+     * example: var a = 1; a = 2.3;
+     */
+    var a: Double = 1.0
+    var b = 2
+
+    a = 2.3
+
+    print(a + b)
+}
