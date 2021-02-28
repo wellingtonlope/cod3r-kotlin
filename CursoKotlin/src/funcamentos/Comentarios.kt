e42b19d0360e9409af8da3475c2953ee96e7bb34
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-02-28 08:13:40 -0400

    comments

---
+++ CursoKotlin/src/funcamentos/Comentarios.kt
@@ -0,0 +1,24 @@
+package funcamentos
+
+/**
+ * Só para dizer que o Kotlin suporta o KDoc... :)
+ *
+ * @param args lista de parâmetros passados por linha de comando
+ */
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+    println("Opa")
+
+    /*
+       Mais de
+       uma linha
+     */
+    println("Sério?")
+
+    /*
+     * Mais de
+     * uma linha
+     * com estilo
+     */
+    println("Legal!")
+}
