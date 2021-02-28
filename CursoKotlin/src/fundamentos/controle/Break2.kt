d421ed0da1acb5ba79af540edda8f2e055aed249
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-02-28 11:55:07 -0400

    using break

---
+++ CursoKotlin/src/fundamentos/controle/Break2.kt
@@ -0,0 +1,11 @@
+package fundamentos.controle
+
+fun main() {
+    externo@ for (i in 1..15) {
+        for (j in 1..15) {
+            if (i == 2 && j == 9) break@externo
+            println("$i $j")
+        }
+    }
+    println("Fim!")
+}
