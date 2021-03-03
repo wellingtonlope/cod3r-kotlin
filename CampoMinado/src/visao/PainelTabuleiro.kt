8b93e4e18f52c5bdc3fcbb3a1f7b726ab45966a6
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-03-03 19:39:51 -0400

    minesweeper

---
+++ CampoMinado/src/visao/PainelTabuleiro.kt
@@ -0,0 +1,15 @@
+package visao
+
+import modelo.Tabuleiro
+import java.awt.GridLayout
+import javax.swing.JPanel
+
+class PainelTabuleiro(tabuleiro: Tabuleiro) : JPanel() {
+    init {
+        layout = GridLayout(tabuleiro.qtdeLinhas, tabuleiro.qtdeColunas)
+        tabuleiro.forEachCampo { campo ->
+            val botao = BotaoCampo(campo)
+            add(botao)
+        }
+    }
+}
